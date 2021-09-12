1fcefae054016cfd3dac665c21c3fca660d14e86
Artem Nikolaev <artem@nikolaev.by>
2021-09-12 23:33:15 +0300

    shoot only if mouse click or space keydown

--- src/hero_shots.js
+++ src/hero_shots.js
@@ -7,11 +7,11 @@ function paintHeroShots(heroShots) {
   });
 }
 
-const { merge, filter, timestamp, sampleTime, combineLatest } = rxjs;
+const { merge, filter, timestamp, sampleTime, combineLatest, distinctUntilKeyChanged } = rxjs;
 const playerFiring = merge(
   fromEvent(canvas, 'click'),
   fromEvent(document, 'keydown')
-    .pipe(filter(evt => evt.keycode === 32)),
+    .pipe(filter(evt => evt.keyCode === 32)),
 )
   .pipe(startWith({}))
   .pipe(sampleTime(200))
@@ -20,9 +20,11 @@ const playerFiring = merge(
 const HeroShots = combineLatest(
   playerFiring, SpaceShip,
   (shotEvents, spaceShip) => ({
+    timestamp: shotEvents.timestamp,
     x: spaceShip.x
   })
 )
+  .pipe(distinctUntilKeyChanged('timestamp'))
   .pipe(scan((shotArray, shot) => {
     shotArray.push({
       x: shot.x,
